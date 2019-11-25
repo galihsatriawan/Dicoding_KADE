6070fc10148cbc2c3e081eee3c99c17c4bd05b3b
galihsatriawan <satriawan.galih99@gmail.com>
2019-11-26 00:43:30 +0700

    - Create Depedency Injection (Contd)

--- FootballLeague/app/src/main/java/id/shobrun/footballleague/dagger/auth/AuthScope.kt
+++ FootballLeague/app/src/main/java/id/shobrun/footballleague/dagger/auth/AuthScope.kt
@@ -1,2 +1,10 @@
 package id.shobrun.footballleague.dagger.auth
 
+import java.lang.annotation.Documented
+import javax.inject.Scope
+
+
+@Scope
+@Documented
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AuthScope
--- FootballLeague/app/src/main/java/id/shobrun/footballleague/ui/LeagueActivity.kt
+++ FootballLeague/app/src/main/java/id/shobrun/footballleague/ui/LeagueActivity.kt
@@ -1,27 +1,18 @@
 package id.shobrun.footballleague.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commitNow
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import dagger.android.support.DaggerAppCompatActivity
 import id.shobrun.footballleague.R
 import id.shobrun.footballleague.views.leagues.fragments.FootballLeaguesFragment
-import id.shobrun.footballleague.views.leagues.fragments.FootballLeaguesViewModel
-import javax.inject.Inject
+
 
 class LeagueActivity : DaggerAppCompatActivity() {
-    @Inject
-    lateinit var viewModelProvider: ViewModelProvider.Factory
 
-    val viewModel:FootballLeaguesViewModel by lazy{
-        ViewModelProviders.of(this,viewModelProvider).get(FootballLeaguesViewModel::class.java)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel.empty
+
         val mClubFragment = FootballLeaguesFragment.getInstance()
         supportFragmentManager.commitNow(allowStateLoss = true){
             add(R.id.frame_container,mClubFragment,FootballLeaguesFragment::class.java.simpleName)
